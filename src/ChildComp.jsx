3b3167bf2127bd48dfe21374b84766d3af9bdd8e
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-16 11:31:52 +0530

    Hooks

---
+++ src/ChildComp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ChildComp() {
+
+console.log("Child Component Rendered")
+  return (
+    <div className='border border-2 border-dark m-2 p-3'>
+      <h1>Child Comp</h1>
+    </div>
+  )
+}
+
+export default React.memo(ChildComp);
---
+++ src/Counter.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+
+function Counter() {
+    var [count,setCountFn] = useState(0);
+
+    // setInterval(() => {
+    //     setCountFn(count + 1);
+    // },2000)
+
+    useEffect(() => {
+        setInterval(() => {
+        setCountFn((cv) => {
+            return cv + 1
+        });
+        console.log("SetInterval Called")
+    },1000)
+    },[])
+
+    function inc(){
+        for(var i=1;i<=4;i++) {
+            setCountFn((cv) => {
+                return cv + 1
+            })
+        }
+    }
+
+    function dec(){
+        setCountFn(count - 1)
+    }
+
+    function res(){
+        setCountFn(0)
+    }
+
+  return (
+    <div className='border border-2 border-dark m-2 p-3'>
+        <h1>Counter:{count}</h1>
+        <button className='me-2' onClick={() => {inc()}}>Increament</button>
+        <button className='me-2' onClick={() => {dec()}}>Decreament</button>
+        <button onClick={() => {res()}}>Reset</button>
+    </div>
+  )
+}
+
+export default Counter
